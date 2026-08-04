59f9324867d5e47fb11ec52c7dea4601ebf18d9f
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-08-04 00:46:04 +0000

    add medical records module

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,39 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
 const authRoutes = require("./routes/authRoutes");
 const appointmentRoutes = require("./routes/appointmentRoutes");
+const medicalRecordRoutes = require("./routes/medicalRecordRoutes");
+
 const connectDB = require("./config/db");
+
 
 dotenv.config();
 
 connectDB();
 
+
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
+
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/appointments", appointmentRoutes);
-const medicalRecordRoutes = require("./routes/medicalRecordRoutes");
+app.use("/api/medical-records", medicalRecordRoutes);
 
-app.use(
-  "/api/medical-records",
-  medicalRecordRoutes
-);
 
 app.get("/", (req, res) => {
   res.send("HealthConnect API is running...");
 });
 
+
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
