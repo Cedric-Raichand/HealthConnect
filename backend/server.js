a7165f6dad2554280574cb384510cbcb4102f5b5
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-08-05 00:55:18 +0000

    add helmet security middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const helmet = require("helmet");
 
 const authRoutes = require("./routes/authRoutes");
 const appointmentRoutes = require("./routes/appointmentRoutes");
@@ -21,7 +22,7 @@ connectDB();
 
 const app = express();
 
-
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
