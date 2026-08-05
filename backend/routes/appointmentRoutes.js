1bc5667165bc216553c2d75d44c4b2d4e042ab91
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-08-05 01:30:58 +0000

    improve on swagger appointment documentation

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+
 const protect = require("../middleware/authMiddleware");
 const authorizeRoles = require("../middleware/roleMiddleware");
-const validate = require("../middleware/validate");
+
 
 const {
   createAppointment,
@@ -12,31 +13,80 @@ const {
 } = require("../controllers/appointmentController");
 
 
-const {
-  createAppointmentValidator,
-} = require("../middleware/validators/appointmentValidator");
 
 
 
-// Patient books appointment
+/**
+ * @swagger
+ * /api/appointments:
+ *   post:
+ *     summary: Book a new appointment
+ *     tags:
+ *       - Appointments
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - doctorId
+ *               - appointmentDate
+ *               - reason
+ *             properties:
+ *               doctorId:
+ *                 type: string
+ *                 example: 64f123456789abcdef123456
+ *               appointmentDate:
+ *                 type: string
+ *                 example: 2026-08-10T10:00:00Z
+ *               reason:
+ *                 type: string
+ *                 example: Regular medical checkup
+ *     responses:
+ *       201:
+ *         description: Appointment booked successfully
+ *       400:
+ *         description: Invalid request
+ *       403:
+ *         description: Only patients can book appointments
+ */
 router.post(
   "/",
   protect,
   authorizeRoles("patient"),
-  createAppointmentValidator,
-  validate,
   createAppointment
 );
 
 
 
-// Patient, Doctor, Admin view appointments
+
+
+
+/**
+ * @swagger
+ * /api/appointments:
+ *   get:
+ *     summary: Get appointments based on user role
+ *     tags:
+ *       - Appointments
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of appointments
+ *       401:
+ *         description: Unauthorized
+ */
 router.get(
   "/",
   protect,
   authorizeRoles("patient", "doctor", "admin"),
   getAppointments
 );
+
 
 
 module.exports = router;
